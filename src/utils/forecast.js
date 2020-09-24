bdef55e2d8c2951eb3e3d22f7df3fe159be42c8f
Rohitkumar19 <tiwari.rohitkumar.14it1025@gmail.com>
2020-09-24 10:08:45 +0530

    Init commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,19 @@
+const request = require('request')
+const forecast = (latitude, longitude, callback) => {
+    const url = 'https://api.darksky.net/forecast/9d1465c6f3bb7a6c71944bdd8548d026/' + latitude + ',' + longitude;
+    request({
+        url: url
+    }, (error, {
+        body
+    }) => {
+        if (error) {
+            callback('Unable to connect to weather service!', undefined)
+        } else if (body.error) {
+            callback('Unable to find location', undefined)
+        } else {
+            callback(undefined, body)
+        }
+    })
+}
+
+module.exports = forecast
---
+++ src/utils/geocode.js
@@ -0,0 +1,22 @@
+const request = require('request')
+const geoCode = (address, callback) => {
+    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(address) + '.json?access_token=pk.eyJ1IjoiYW5kcmV3bWVhZDEiLCJhIjoiY2pvOG8ybW90MDFhazNxcnJ4OTYydzJlOSJ9.njY7HvaalLEVhEOIghPTlw&limit=1';
+    console.log('url', url)
+    request({
+        url
+    }, (error, response) => {
+        if (error) {
+            callback('Unable to connect to weather service!', undefined)
+        } else if (response.body) {
+            callback('Unable to find location. Try another search.', undefined)
+        } else {
+            callback(undefined, {
+                location: '124',
+                logitude: '125',
+                loaction: 'United States'
+            })
+        }
+    })
+}
+
+module.exports = geoCode
